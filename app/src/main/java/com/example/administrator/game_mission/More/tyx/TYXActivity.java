166847c06d8480cd13894cd5b47db8875ff78ba1
Lhy <Lhy@mindmini.com>
2020-08-09 17:12:10 +0800

    修改骰子dialog不可点外面

--- app/src/main/java/com/example/administrator/game_mission/More/tyx/TYXActivity.java
+++ app/src/main/java/com/example/administrator/game_mission/More/tyx/TYXActivity.java
@@ -102,6 +102,7 @@ public class TYXActivity extends AppCompatActivity implements View.OnClickListen
                 builder.setView(view1);
                 builder.create();
                 final AlertDialog dialog = builder.show();
+                dialog.setCanceledOnTouchOutside(false);
                 final EditText ed1 = view1.findViewById(R.id.ed1);
                 final EditText ed2 = view1.findViewById(R.id.ed2);
                 final EditText ed3 = view1.findViewById(R.id.ed3);
